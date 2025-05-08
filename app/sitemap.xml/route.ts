d3183e1fee75e0aa0048edc9425f9fa840edcf1a
yousef-yaghoubi <yousef1307y@gmail.com>
2025-05-07 23:40:53 -0700

    try to fix sitemap.xml

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,12 +1,8 @@
 import { NextResponse } from 'next/server'
-import prisma from '@/prisma/prismaClient'
 
 const BASE_URL = 'https://tarkhineh-1307.vercel.app'
 
 export async function GET() {
-  const foods = await prisma.foods.findMany({ select: { id: true } })
-  const branches = await prisma.branchs.findMany({ select: { id: true } })
-
   const staticRoutes = [
     '',
     '/about',
@@ -24,27 +20,9 @@ export async function GET() {
     </url>`
   )
 
-  const foodXML = foods.map(
-    (food) => `
-    <url>
-      <loc>${BASE_URL}/menu/${food.id}</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.7</priority>
-    </url>`
-  )
-
-  const branchXML = branches.map(
-    (branch) => `
-    <url>
-      <loc>${BASE_URL}/branches/${branch.id}</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.6</priority>
-    </url>`
-  )
-
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${[...staticXML, ...foodXML, ...branchXML].join('')}
+    ${[...staticXML].join('')}
   </urlset>`
 
   return new NextResponse(xml, {
